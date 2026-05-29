0a7549917113cf6e263d0105d962088e618c145a
hiago <hiago232@hotmail.com>
2026-05-28 21:51:33 -0300

    feat: implementa PedidoCompraItemService

--- src/main/java/com/gestaooserp/dev/controller/PedidoCompraItemController.java
+++ src/main/java/com/gestaooserp/dev/controller/PedidoCompraItemController.java
@@ -8,12 +8,61 @@ package com.gestaooserp.dev.controller;
  */
 
 import com.gestaooserp.dev.entity.PedidoCompraItem;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gestaooserp.dev.service.PedidoCompraItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pedido-compra/item")
 public class PedidoCompraItemController {
 
-    private final PedidoCompraItem
+    private final PedidoCompraItemService pedidoCompraItemService;
+
+    public PedidoCompraItemController(PedidoCompraItemService pedidoCompraItemService){
+        this.pedidoCompraItemService = pedidoCompraItemService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<PedidoCompraItem>> getAll(){
+        List<PedidoCompraItem> pedidoCompraItemList = pedidoCompraItemService.findAll();
+        if (pedidoCompraItemList != null){
+            return new ResponseEntity<>(pedidoCompraItemList, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null,HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoCompraItem> getById(@PathVariable Long id){
+        PedidoCompraItem pedidoCompraItem = pedidoCompraItemService.findById(id);
+        if (pedidoCompraItem != null){
+            return new ResponseEntity<>(pedidoCompraItem,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<PedidoCompraItem> create(@RequestBody PedidoCompraItem pedidoCompraItem){
+        return new ResponseEntity<>(pedidoCompraItemService.save(pedidoCompraItem),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PedidoCompraItem> update(@PathVariable Long id,@RequestBody PedidoCompraItem pedidoCompraItem){
+        PedidoCompraItem pedidoCompraItemAtualizado = pedidoCompraItemService.update(id,pedidoCompraItem);
+        if (pedidoCompraItemAtualizado != null){
+            return new ResponseEntity<>(pedidoCompraItemAtualizado,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable Long id){
+        if (pedidoCompraItemService.delete(id)){
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(false,HttpStatus.NOT_FOUND);
+    }
+
 }
