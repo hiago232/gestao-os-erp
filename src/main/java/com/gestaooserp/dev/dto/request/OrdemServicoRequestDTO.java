4d481456bd3c98f6f7064b11be23c069fdfae76e
hiago <hiago232@hotmail.com>
2026-06-08 18:37:11 -0300

    feat: implementa OrdemServicoResponseDTO

--- src/main/java/com/gestaooserp/dev/dto/request/OrdemServicoRequestDTO.java
+++ src/main/java/com/gestaooserp/dev/dto/request/OrdemServicoRequestDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 public record OrdemServicoRequestDTO(
 
         @NotNull
-        Long funcionarioId,
+        Integer funcionarioId,
 
         @NotNull
         Long clienteId,
--- src/main/java/com/gestaooserp/dev/dto/response/OrdemServicoResponseDTO.java
+++ src/main/java/com/gestaooserp/dev/dto/response/OrdemServicoResponseDTO.java
@@ -1,4 +1,12 @@
 package com.gestaooserp.dev.dto.response;
 
-public record OrdemServicoResponseDTO() {
+public record OrdemServicoResponseDTO(
+        Long ordemServicoId,
+        Integer funcionarioId,
+        Long clienteId,
+        Long manutencaoId,
+        Long equipamentoId,
+        Long servicoLocalId
+
+) {
 }
