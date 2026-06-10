dc0d9bf48c94d30e50fbadc551e1c1370f07a5e3
hiago <hiago232@hotmail.com>
2026-06-10 17:20:43 -0300

    refactor: adapta metodo save em EquipamentoService

--- src/main/java/com/gestaooserp/dev/service/EquipamentoService.java
+++ src/main/java/com/gestaooserp/dev/service/EquipamentoService.java
@@ -8,10 +8,12 @@ package com.gestaooserp.dev.service;
 
 import com.gestaooserp.dev.dto.request.EquipamentoRequestDTO;
 import com.gestaooserp.dev.dto.response.EquipamentoResponseDTO;
+import com.gestaooserp.dev.dto.response.FabricanteResponseDTO;
 import com.gestaooserp.dev.entity.Cliente;
 import com.gestaooserp.dev.entity.Equipamento;
 import com.gestaooserp.dev.entity.Fabricante;
 import com.gestaooserp.dev.repository.EquipamentoRepository;
+import com.gestaooserp.dev.repository.FabricanteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,16 +24,16 @@ public class EquipamentoService {
 
     private final EquipamentoRepository equipamentoRepository;
     private final ClienteService clienteService;
-    private final FabricanteService fabricanteService;
+    private final FabricanteRepository fabricanteRepository;
 
     public EquipamentoService(
             EquipamentoRepository equipamentoRepository,
             ClienteService clienteService,
-            FabricanteService fabricanteService
+            FabricanteRepository fabricanteRepository
     ){
         this.equipamentoRepository = equipamentoRepository;
         this.clienteService = clienteService;
-        this.fabricanteService = fabricanteService;
+        this.fabricanteRepository = fabricanteRepository;
     }
 
     public List<EquipamentoResponseDTO> findAll(){
@@ -46,7 +48,7 @@ public class EquipamentoService {
     public EquipamentoResponseDTO save(EquipamentoRequestDTO requestDTO){
         Equipamento equipamento = new Equipamento();
         Cliente cliente = clienteService.findById(requestDTO.clienteId());
-        Fabricante fabricante = fabricanteService.findById(requestDTO.fabricanteId());
+        Fabricante fabricante = fabricanteRepository.findById(requestDTO.fabricanteId()).orElse(null);
         return new EquipamentoResponseDTO(equipamentoRepository.save(updateEntity(
                 requestDTO,
                 equipamento,
