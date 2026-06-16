6aae4aa40cca8e9f589b4f8f5723993a18367241
hiago <hiago232@hotmail.com>
2026-06-16 16:55:58 -0300

    feat: implementa documentacao inicial da API em FuncionarioController

--- src/main/java/com/gestaooserp/dev/controller/FuncionarioController.java
+++ src/main/java/com/gestaooserp/dev/controller/FuncionarioController.java
@@ -9,6 +9,9 @@ import com.gestaooserp.dev.dto.request.FuncionarioRequestDTO;
 import com.gestaooserp.dev.dto.response.FuncionarioResponseDTO;
 import com.gestaooserp.dev.entity.Funcionario;
 import com.gestaooserp.dev.service.FuncionarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +23,20 @@ import java.util.List;
 @RequestMapping("/funcionario")
 public class FuncionarioController {
 
+
     private final FuncionarioService funcionarioService;
 
     public FuncionarioController(FuncionarioService funcionarioService){
         this.funcionarioService = funcionarioService;
     }
 
+    @Operation(summary="Listar todos funcionarios",description = "Listagem de funcionarios")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "A requisição foi executada com sucesso."),
+            @ApiResponse(responseCode = "400",description = "Requisição inválida"),
+            @ApiResponse(responseCode = "403",description = "Você não tem permissão.")
+
+    })
     @GetMapping("/")
     public ResponseEntity<List<FuncionarioResponseDTO>> getAll(){
         List<FuncionarioResponseDTO> funcionarioList = funcionarioService.findAll();
