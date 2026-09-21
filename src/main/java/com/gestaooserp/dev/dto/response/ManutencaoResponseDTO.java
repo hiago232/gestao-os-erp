6b02a989ee5701dd667c9e665689cf63fa590241
hiago <hiago232@hotmail.com>
2026-09-21 20:46:52 -0300

    reafactor: adciona atributos a ManutencaoResponseDTO

--- src/main/java/com/gestaooserp/dev/dto/response/ManutencaoResponseDTO.java
+++ src/main/java/com/gestaooserp/dev/dto/response/ManutencaoResponseDTO.java
@@ -6,15 +6,33 @@ import java.time.LocalDate;
 
 public record ManutencaoResponseDTO(
         Long manutencaoId,
-        Integer status,
-        LocalDate dataEntrada
-) {
+        Integer funcionarioId,
+        Long clienteId,
+        Long equipamentoId,
+        String status,
+        String problemaRelatado,
+        String defeitoConstatado,
+        String servicoRealizado,
+        LocalDate dataEntrada,
+        LocalDate dataInicial,
+        LocalDate dataFinal,
+        LocalDate dataSaida
+        ) {
 
     public  ManutencaoResponseDTO(Manutencao manutencao){
         this(
                 manutencao.getManutencaoId(),
-                manutencao.getOrdemServico().getStatus().getCodigo(),
-                manutencao.getDataEntrada()
+                manutencao.getOrdemServico().getFuncionario().getFuncionarioId(),
+                manutencao.getOrdemServico().getCliente().getClienteId(),
+                manutencao.getOrdemServico().getEquipamento().getEquipamentoId(),
+                manutencao.getOrdemServico().getStatus().name(),
+                manutencao.getProblemaRelatado(),
+                manutencao.getDefeitoConstatado(),
+                manutencao.getServicoRealizado(),
+                manutencao.getDataEntrada(),
+                manutencao.getDataInicial(),
+                manutencao.getDataFinal(),
+                manutencao.getDataSaida()
         );
     }
 }
